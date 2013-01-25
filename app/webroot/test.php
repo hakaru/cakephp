b463c96f858701e6fcfeb26ac742b9dc7304524b
Hakaru Hirose <hakaru@fancs.com>
2013-01-25 14:46:21 +0900

    edit CAKE_CORE_INCLUDE_PATH

--- app/webroot/test.php
+++ app/webroot/test.php
@@ -54,6 +54,8 @@ if (!defined('APP_DIR')) {
  * Leaving this constant undefined will result in it being defined in Cake/bootstrap.php
  */
 	//define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'lib');
+define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'cakephp' . DS . 'lib');
+
 
 /**
  * Editing below this line should not be necessary.
